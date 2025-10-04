8cd941395568ab243be02e6c32d62f60b45deefc
Izael Gomes <izaelgomes127@gmail.com>
2025-10-04 18:14:09 -0300

    feat: movie details

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,19 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import './global.css';
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
+      <StatusBar hidden={true} />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-        <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="movie/[id]"
+          options={{ headerShown: false, animation: 'fade' }}
+        />
       </Stack>
     </SafeAreaProvider>
   );
